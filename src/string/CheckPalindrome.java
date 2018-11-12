184527939888aff7ab113b4425d3261e3457a54d
studiolee <1964773741@qq.com>
2018-11-12 21:16:45 +0800

    验证回文串

---
+++ src/string/CheckPalindrome.java
@@ -0,0 +1,45 @@
+package string;
+
+import org.junit.Test;
+
+/**
+ * 125. 验证回文串 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * 
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ * 
+ * 示例 1: 输入: "A man, a plan, a canal: Panama" 输出: true
+ * 
+ * 示例 2: 输入: "race a car" 输出: false
+ */
+public class CheckPalindrome {
+	public boolean isPalindrome(String s) {
+		if(s.length() == 0 || s.length() == 1) return true;
+		return check(s);
+	}
+	
+	//写判定回文的方法
+	private boolean check(String s){
+		s = s.toLowerCase();
+		int len = s.length();
+		int low = 0, high = len - 1;
+		while(low <= high){			//这里=的情况是长度小于2时的
+			if(s.charAt(low) == s.charAt(high)){
+				low++; high--;
+			}else if(!Character.isLetterOrDigit(s.charAt(low))){
+				low++;
+			}else if(!Character.isLetterOrDigit(s.charAt(high))){
+				high--;
+			}else{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Test
+	public void test1(){
+		String s = "A man, a plan, a canal: Panama";
+		String s1 = "a.";
+		System.out.println(isPalindrome(s1));
+	}
+}
