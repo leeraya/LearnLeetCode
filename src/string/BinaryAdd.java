e3bec560e872b6be1d07a5af1cccd698a655608a
studiolee <1964773741@qq.com>
2018-11-27 17:05:45 +0800

    11/27添加注释

--- src/string/BinaryAdd.java
+++ src/string/BinaryAdd.java
@@ -1,5 +1,7 @@
 package string;
 
+import org.junit.Test;
+
 /**
  * @author leeray
  * @version 创建时间：2018年11月27日 下午3:37:49 <br>
@@ -16,22 +18,30 @@ package string;
  *          输入: a = "1010", b = "1011" 输出: "10101"
  */
 public class BinaryAdd {
+	/*
+	 * 总的思路是从低位向高位逐级相加，期间保存数值和对应的进位
+	 */
 	public String addBinary(String a, String b) {
 		int i = a.length() - 1, j = b.length() - 1;
 		StringBuilder sb = new StringBuilder();
 		int sum = 0, carry = 0; // sum % 2表示某位上的结果，sum / 2表示进位。
 		while (i >= 0 || j >= 0) {
-			sum = carry;
+			sum = carry;	//carry是进位，存储前一次相加的结果的溢出
 			if (j >= 0)
-				sum += b.charAt(j--) - '0';
+				sum += b.charAt(j--) - '0';		//一个char值 - '0' 等于对应的ascall码，例如：'a' - '0' = 49 ;
 			if (i >= 0)
 				sum += a.charAt(i--) - '0';
 
-			sb.append(sum % 2);
-			carry = sum / 2;
+			sb.append(sum % 2);	
+			carry = sum / 2;	//求进位
 		}
-		if (carry != 0)
+		if (carry != 0)	//判断最后有没有溢出，如果有，sb就要加上这个溢出的值
 			sb.append(carry);
-		return sb.reverse().toString();
+		return sb.reverse().toString();		//StringBuilder的reverse()可以反转StringBuilder的内容
+	}
+	
+	@Test
+	public void test1(){
+		System.out.println('a' - '0');
 	}
 }
