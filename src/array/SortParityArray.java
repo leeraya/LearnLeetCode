85e77e34a0cd27c5d447a728c8eadd01117063ee
studiolee <1964773741@qq.com>
2018-12-20 12:53:47 +0800

    update on 12/20

--- src/array/SortParityArray.java
+++ src/array/SortParityArray.java
@@ -18,16 +18,21 @@ public class SortParityArray {
 		if (A == null || A.length == 0)
 			return A;
 		int len = A.length;
-		int temp1[] = new int[len + 1];
-		int temp2[] = new int[len + 1];
+		int temp1[] = new int[len];
+		int temp2[] = new int[len];
 		int index1 = 0, index2 = 0;
 		for (int i = 0; i < len; i++) {
 			if (A[i] % 2 == 0) {
+				//temp1装偶数元素
 				temp1[index1++] = A[i];
 			} else {
+				//temp2装奇数元素
 				temp2[index2++] = A[i];
 			}
 		}
+		//分别指向数组temp1,temp2的索引要重新归零。
+		//其实不归零也行，只要在下面的循环中每次减小索引就行，相当于从后往前装入即:
+		//--index1;--index2;注意要先减
 		index1 = 0;
 		index2 = 0;
 		int res[] = new int[len];
@@ -37,6 +42,7 @@ public class SortParityArray {
 			if (i % 2 == 0) {
 				res[i] = temp1[index1++];
 			} else {
+				//如果碰到奇数索引，就从奇数数组中取数装入
 				res[i] = temp2[index2++];
 			}
 		}
