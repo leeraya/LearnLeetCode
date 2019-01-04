27dd75798db60a8fdd290c88a27ac1408f0545bb
studiolee <1964773741@qq.com>
2019-01-04 17:01:23 +0800

    update on 1/4

---
+++ src/stack/BaseBall.java
@@ -0,0 +1,45 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @author leeray
+ * @version 创建时间：2019年1月4日 下午4:58:10 <br>
+ *          描述：682. 棒球比赛<br>
+ *          你现在是棒球比赛记录员。 给定一个字符串列表，每个字符串可以是以下四种类型之一：
+ *          1.整数（一轮的得分）：直接表示您在本轮中获得的积分数。 2. "+"（一轮的得分）：表示本轮获得的得分是前两轮有效 回合得分的总和。
+ *          3. "D"（一轮的得分）：表示本轮获得的得分是前一轮有效 回合得分的两倍。 4.
+ *          "C"（一个操作，这不是一个回合的分数）：表示您获得的最后一个有效 回合的分数是无效的，应该被移除。
+ * 
+ *          每一轮的操作都是永久性的，可能会对前一轮和后一轮产生影响。 你需要返回你在所有回合中得分的总和。
+ */
+public class BaseBall {
+	public int calPoints(String[] ops) {
+		Stack<Integer> s = new Stack<Integer>();
+		for (String str : ops) {
+			switch (str) {
+			case "+":
+				int temp1 = s.pop();
+				int temp2 = s.pop();
+				s.push(temp2);
+				s.push(temp1);
+				s.push(temp1 + temp2);
+				break;
+			case "D":
+				s.push(s.peek() * 2);
+				break;
+			case "C":
+				s.pop();
+				break;
+			default:
+				s.push(Integer.parseInt(str));
+				break;
+			}
+		}
+		int res = 0;
+		while (!s.isEmpty()) {
+			res += s.pop();
+		}
+		return res;
+	}
+}
--- src/stack/EvalRPN.java
+++ src/stack/EvalRPN.java
@@ -1,4 +1,4 @@
-package math;
+package stack;
 
 import java.util.Stack;
 
