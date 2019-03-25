dfc2f60c70dd8d232a02f4da228e4d92adf5ed29
studiolee <1964773741@qq.com>
2019-03-25 22:52:08 +0800

    update on 3/15

--- src/tree/PostOrder.java
+++ src/tree/PostOrder.java
@@ -5,20 +5,31 @@ import java.util.List;
 
 /**
  * @author leeray
- * @version 创建时间：2019年3月23日 下午6:31:36 <br>
- *          描述：590. N叉树的后序遍历<br>
+ * @version 创建时间：2019年3月25日 下午10:42:30 <br>
+ * 			描述：145. 二叉树的后序遍历<br>
+ * 给定一个二叉树，返回它的 后序 遍历。
+
+		示例:
+		
+		输入: [1,null,2,3]  
+		   1
+		    \
+		     2
+		    /
+		   3 
+		
+		输出: [3,2,1]
+		进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 public class PostOrder {
-	List<Integer> list = new ArrayList<Integer>();
+	List<Integer> res = new ArrayList<Integer>();
 
-	public List<Integer> postorder(Node root) {
+	public List<Integer> postorderTraversal(TreeNode root) {
 		if (root == null)
-			return list;
-		for (Node n : root.children) {
-			if (n != null)
-				postorder(n);
-		}
-		list.add(root.val);
-		return list;
+			return res;
+		postorderTraversal(root.left);
+		postorderTraversal(root.right);
+		res.add(root.val);
+		return res;
 	}
 }
