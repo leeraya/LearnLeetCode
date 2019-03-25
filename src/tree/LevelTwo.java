8f654b01b3e33cee39fb6e96c2e24b77874be9da
studiolee <1964773741@qq.com>
2019-03-25 21:59:40 +0800

    update on 3/25

--- src/tree/LevelTwo.java
+++ src/tree/LevelTwo.java
@@ -8,12 +8,13 @@ import java.util.Queue;
 /**
  * @author leeray
  * @version 创建时间：2019年3月25日 下午9:51:59 <br>
- * 			描述：<br>
+ *          描述：107. 二叉树的层次遍历 II<br>
+ *          给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
  */
 public class LevelTwo {
 	public List<List<Integer>> levelOrderBottom(TreeNode root) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
-		List<List<Integer>> temp = new ArrayList<List<Integer>>();//存储正序遍历结果
+		List<List<Integer>> temp = new ArrayList<List<Integer>>();// 存储正序遍历结果
 		if (root == null)
 			return res;
 		Queue<TreeNode> Q = new LinkedList<TreeNode>();
